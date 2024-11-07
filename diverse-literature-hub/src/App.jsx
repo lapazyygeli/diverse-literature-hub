27633bad4389a1b0524f75a49d4a298e4ecd980a
lapazyygeli <137160486+lapazyygeli@users.noreply.github.com>
2024-11-07 15:45:28 +0200

    Add hero section / navbar

--- diverse-literature-hub/src/App.jsx
+++ diverse-literature-hub/src/App.jsx
@@ -1,36 +1,43 @@
 // import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <header>
-        hjkl
+        <div className="hero-image">
+          <nav className="navbar">
+            <ul>
+              <li><a href="#">home</a></li>
+              <li><a href="#title-author">search</a></li>
+              <li><a href="#categories">categories</a></li>
+              <li><a href="#latest-releases">latest releases</a></li>
+              <li><a href="#">about</a></li>
+            </ul>
+          </nav>
+        </div>
       </header>
       <main>
-        <section className='title-author'>
-          <div className='container'>
+        <section id="title-author" className="title-author">
+          <div className="container">
             <h2>explore books by title or author</h2>
           </div>
         </section>
-        <section className='categories'>
-          <div className='container'>
+        <section id="categories" className="categories">
+          <div className="container">
             <h2>explore books by categories</h2>
           </div>
         </section>
-        <section className='latesta-releases'>
-          <div className='container'>
+        <section id="latest-releases" className="latest-releases">
+          <div className="container">
             <h2>latest releases</h2>
           </div>
         </section>
       </main>
-      <footer>
-        footer
-      </footer>
+      <footer>footer</footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
