541b97192f79e5a68d0656fec9dc68dc8f5cd081
lapazyygeli <137160486+lapazyygeli@users.noreply.github.com>
2024-11-27 15:41:09 +0200

    Modify title-author section

--- diverse-literature-hub/src/App.jsx
+++ diverse-literature-hub/src/App.jsx
@@ -9,11 +9,21 @@ function App() {
         <div className="hero-image">
           <nav className="navbar">
             <ul>
-              <li><a href="#">home</a></li>
-              <li><a href="#title-author">search</a></li>
-              <li><a href="#categories">categories</a></li>
-              <li><a href="#latest-releases">latest releases</a></li>
-              <li><a href="#">about</a></li>
+              <li>
+                <a href="#">home</a>
+              </li>
+              <li>
+                <a href="#title-author">search</a>
+              </li>
+              <li>
+                <a href="#categories">categories</a>
+              </li>
+              <li>
+                <a href="#latest-releases">latest releases</a>
+              </li>
+              <li>
+                <a href="#">about</a>
+              </li>
             </ul>
           </nav>
         </div>
@@ -22,6 +32,35 @@ function App() {
         <section id="title-author" className="title-author">
           <div className="container">
             <h2>explore books by title or author</h2>
+            <div className="introduction-wrapper">
+              <div className="search-bar-avatar-wrapper">
+                <div className="search-bar">
+                  <button>
+                    <img
+                      src="/src/assets/images/icons/search.png"
+                      alt="search icon"
+                    />
+                  </button>
+                  <input type="text" />
+                </div>
+                <div className="avatar-wrapper">
+                  <div className="real-avatar-wrapper">
+                    <img
+                      src="/src/assets/images/michael-dam-mEZ3PoFGs_k-unsplash.jpg"
+                      alt="avatar image"
+                    />
+                  </div>
+                  <p>
+                    - “Narratives that inspire, inform, and transport you to
+                    distant lands, all woven together by the threads of
+                    compelling authorship and storytelling.”
+                  </p>
+                </div>
+              </div>
+              <div className="image-wrapper">
+                <img src="/src/assets/images/ying-ge--Yo1cWJVKFY-unsplash.jpg" />
+              </div>
+            </div>
           </div>
         </section>
         <section id="categories" className="categories">
